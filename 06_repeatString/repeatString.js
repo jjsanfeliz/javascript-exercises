e29288534975240925b4b3d7ad089292d2f46c9c
jjsanfeliz <58214571+jjsanfeliz@users.noreply.github.com>
2025-05-10 09:52:36 -0600

    Add Solution to Ex06, Run Tests Modify Test (spec) document ro run all Tests Modify .js document to add function for repeating strings Modify .js document to add function to throw error if < 0

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str,times) {
+    if (times < 0) {
+        return "ERROR";
+    }else{
+        var Nstr = '';
+        for (let i = 0; i < times; i++) {
+            Nstr = Nstr + str;
+        }
+        return Nstr;
+    } 
 };
 
 // Do not edit below this line
