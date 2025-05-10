e485f395a1228a5579e730c034e96008bb3d3ddf
jjsanfeliz <58214571+jjsanfeliz@users.noreply.github.com>
2025-05-10 10:44:24 -0600

    Add Solution, run tests ex07 Modify test code, run all test (.spec) Modify .js, add functions to reverse string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,22 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    //Declare new Array, will recieve string characters
+    let strArr = [];
+    //Add string characters to array
+    for (const i in str) {
+        strArr[i] = str[i]
+    }
+    //Declare Array for reversed characters
+    let revArr = [];
+    //Populate Array with characters reversed
+    for (let i = 1; i <= strArr.length; i++) {
+            revArr[i] = strArr[strArr.length-i];
+    }
+    //Eliminate first, empty element
+    revArr.shift();
+    
+    //Create new strin joining elements in reversed array
+    const Nstr = revArr.join('');
+    return Nstr;
 };
 
 // Do not edit below this line
