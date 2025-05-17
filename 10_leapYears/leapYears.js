fd235c919b20413875baee1ed4ab554a8bd50e42
jjsanfeliz <58214571+jjsanfeliz@users.noreply.github.com>
2025-05-16 19:01:05 -0600

    Add solution, run tests ex10 modify js file with if functions modify spec file to run all tests

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    var lyear = false;
+    if (year % 4 === 0) {
+      lyear = true;  
+    }
+        if (year % 4 === 0 && year % 100 === 0) {
+        lyear = false;
+    }
+    if (year % 4 === 0 && year % 400 === 0) {
+        lyear = true;
+    }
+    return lyear;
 };
 
 // Do not edit below this line
