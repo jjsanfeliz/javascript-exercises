326204d8454bb926595b638d3a94fa933fb04f75
jjsanfeliz <58214571+jjsanfeliz@users.noreply.github.com>
2025-05-18 13:03:24 -0600

    add solution ex 13, run all tests Add functions to check if palindrome in .js file Delete ".skip" in .spec.js file, to run all test

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const NPSword = removePunctuationAndSpaces(word);
+    const NPSLword = NPSword.toLowerCase();
+    const Rword = reverseString(NPSLword);
+    if (Rword === NPSLword) {
+        return true;
+    }else{
+        return false;
+    }
 
 };
+const reverseString = function (string) {
+    
+  return string.split("").reverse().join("");
+};
 
+function removePunctuationAndSpaces(str) {
+  return str.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "");
+}
 // Do not edit below this line
 module.exports = palindromes;
