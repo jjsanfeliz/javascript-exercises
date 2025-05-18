f5c4328f08e840156ea6e713c93b0a369cdb85fa
jjsanfeliz <58214571+jjsanfeliz@users.noreply.github.com>
2025-05-18 11:58:05 -0600

    Add solution Ex11, run all tests Add functions to convert temperatures Remove test skips, to run all tests.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+function roundToDecimal(num, decimalPlaces) {
+      const factor = 10 ** decimalPlaces;
+      return Math.round(num * factor) / factor;
+    }
+
+const convertToCelsius = function(temp) {
+  temp = (temp -32)/(9/5)
+  return roundToDecimal(temp,1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+   temp = (temp * (9/5)) + 32;
+   return roundToDecimal(temp,1);
 };
 
 // Do not edit below this line
