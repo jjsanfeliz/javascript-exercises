3834037a9ae110358276e4ef3a3c8af6aac38881
jjsanfeliz <58214571+jjsanfeliz@users.noreply.github.com>
2025-05-18 12:40:34 -0600

    Add solution Ex12, run all tests Add functions for calculator.js Delete .skip from spec.js to run all tests

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(n1,n2) {
+	return n1+n2
+};
+const subtract = function(n1,n2) {
+	return n1-n2
+};
+const sum = function(arr) {
+  let res = 0;
+	for (num of arr) {
+    res = res + num;
+  }
+  if (res === undefined)
+  {
+    return 0
+  }
+  return res;
 };
 
-const subtract = function() {
-	
+const multiply = function(arr) {
+  let res = 1;
+	for (num of arr) {
+    res = res * num;
+  }
+  if (res === undefined)
+  {
+    return 0
+  }
+  return res;
 };
 
-const sum = function() {
-	
+const power = function(n1,n2) {
+	return n1 ** n2;
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+  res = 1;
+	for (let index = n; index > 0; index--) {
+    res = res * index;
+  }
+  return res;
 };
 
 // Do not edit below this line
